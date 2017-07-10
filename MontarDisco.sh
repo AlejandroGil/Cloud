8a45bfb9357249603e3b1fcac24a811eb37e8f2e
Alejandro Gil <alejandro.gilh@estudiante.uam.es>
2017-07-10 15:08:50 +0200

    Create MontarDisco.sh

---
+++ MontarDisco.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+(
+echo n
+echo  
+echo  
+echo  
+echo  
+echo w
+) | sudo fdisk /dev/sdc
+sudo mkfs -t ext4 /dev/sdc1
+sudo mkdir /datadrive
+sudo mount /dev/sdc1 /datadrive
+UUID=``sudo -i blkid | grep sdc1 | awk -F'"' '{print $2}'
+fstab="UUID=$UUID /datadrive ext4 defaults 1 2"
+sudo echo  $fstab >> /etc/fstab
+sudo chmod go+w /datadrive
