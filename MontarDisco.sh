7a8f4bd8896dd5581e057df57a5af7044649de23
Alejandro Gil <alejandro.gilh@estudiante.uam.es>
2017-07-14 14:45:18 +0200

    Update MontarDisco.sh

--- MontarDisco.sh
+++ MontarDisco.sh
@@ -10,7 +10,7 @@ echo w
 sudo mkfs -t ext4 /dev/sdc1
 sudo mkdir /datadrive
 sudo mount /dev/sdc1 /datadrive
-UUID=``sudo -i blkid | grep sdc1 | awk -F'"' '{print $2}'
+UUID=`sudo -i blkid | grep sdc1 | awk -F'"' '{print $2}'`
 fstab="UUID=$UUID /datadrive ext4 defaults 1 2"
 sudo echo  $fstab >> /etc/fstab
 sudo chmod go+w /datadrive
