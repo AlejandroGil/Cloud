a8b89c295323eae7c9c8bdf0195bd4d506555f8e
Alejandro Gil <alejandro.gilh@estudiante.uam.es>
2018-03-28 10:08:22 +0200

    Update index.js

--- Functions/PhoneCall/index.js
+++ Functions/PhoneCall/index.js
@@ -5,18 +5,18 @@ const rp = require('request-promise');
 module.exports = function (context, inputBlob) {
     context.log("-------- Twilio phone call --------");
     
-    var blobURL = "https://minsaitocrpoc.blob.core.windows.net/voice-templates/" + context.bindingData.blob;
+    var blobURL = "" + context.bindingData.blob;
     
     //Leer de keyVault
-    const accountSid = 'AC861f071b53f8c69f36f0b941b6affb8a';
-    const authToken = '0849cec7b05abf185771f779c52bbd5e';
+    const accountSid = '';
+    const authToken = '';
     const client = require('twilio')(accountSid, authToken);
 
     client.calls
     .create({
         url: blobURL,
-        to: '+34914808359',
-        from: '+34911984521',
+        to: '+',
+        from: '+',
         method: 'GET',
     });
     context.done();
