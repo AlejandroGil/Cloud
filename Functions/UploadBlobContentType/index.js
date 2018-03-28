7ad310b3657375e0da5520b56a059e8b3bd5a05a
Alejandro Gil <alejandro.gilh@estudiante.uam.es>
2018-03-28 10:11:53 +0200

    Update index.js

--- Functions/UploadBlobContentType/index.js
+++ Functions/UploadBlobContentType/index.js
@@ -6,7 +6,7 @@ module.exports = function (context, inputBlob) {
     var content = '<?xml version="1.0" encoding="UTF-8"?><Response><Say voice="alice" language="es-ES">' + inputBlob + '</Say></Response>' 
     var options = {contentSettings:{contentType:'application/xml'}}
     var outputFileName = context.bindingData.blobname + ".xml"; 
-    var blobService = azure.createBlobService("DefaultEndpointsProtocol=https;AccountName=minsaitocrpoc;AccountKey=GwltcsURZD1q0wkYEvU5iXiIrTeit6Xy4adq5PznP8XwX8ugB/b78cD7PJhdStpcTB/ZWhjQHoVc+VLMWoJ0MA==;BlobEndpoint=https://minsaitocrpoc.blob.core.windows.net/;");
+    var blobService = azure.createBlobService("--");
 
     content_stream = streamifier.createReadStream(content);
     content_stream.pipe(blobService.createWriteStreamToBlockBlob("voice-templates", outputFileName, options, function(error, result, response) {
