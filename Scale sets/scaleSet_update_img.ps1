8992bcf8f243ef9019118faf31126d4afc2bcf48
AlejandroGil <agilhernan@gmail.com>
2018-04-19 13:02:37 +0200

    Updated VMSS image

--- Scale sets/scaleSet_update_img.ps1
+++ Scale sets/scaleSet_update_img.ps1
@@ -1,11 +1,12 @@
+﻿Add-AzureRmAccount
+
 $rgname = "xxx"
 $vmssname = "xxx"
-$newImageReference = "/subscriptions/xxx"
 
 #Image from napshot params
-$snapshotName = "config-snapshot-test"
+$snapshotName = "xxx"
 $location = "westeurope"
-$imageName = "config-image-test"
+$imageName = "xxx"
 
 Set-AzureRmContext -SubscriptionName "xxx"
 
@@ -17,10 +18,12 @@ $imageConfig = New-AzureRmImageConfig -Location $location
 $imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType Linux -SnapshotId $snapshot.Id
 $rmImage = New-AzureRmImage -ImageName $imageName -ResourceGroupName $rgName -Image $imageConfig
 
+#En caso que la imagen exista:
+#$rmImage = Get-AzureRmImage -ImageName $imageName -ResourceGroupName $rgname
+
 #################################
 ##       Update the vmss       ##
 #################################
 $vmss = Get-AzureRmVmss -ResourceGroupName $rgname -VMScaleSetName $vmssname
 $vmss.virtualMachineProfile.storageProfile.imageReference.id = $rmImage.id
 Update-AzureRmVmss -ResourceGroupName $rgname -Name $vmssname -VirtualMachineScaleSet $vmss
-Update-AzureRmVmss -ResourceGroupName $rgname -VirtualMachineScaleSet $vmss
