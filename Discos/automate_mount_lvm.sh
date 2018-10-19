7d19317bda465baaa65748db67ea7030ddb414ac
valonsoa <valoalv2@gmail.com>
2018-10-19 11:38:42 +0200

    Update automate_mount_lvm.sh

--- Discos/automate_mount_lvm.sh
+++ Discos/automate_mount_lvm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-physical_device=sudo grep SCSI /var/log/syslog | tail -1 | grep -i attached | awk '{print $10}' | tr -d []
+physical_device=$(sudo grep SCSI /var/log/syslog | tail -1 | grep -i attached | awk '{print $10}' | tr -d [])
 
 sudo apt-get install -y lvm2
-sudo vgcreate vg1 /dev/${physical_device}
+sudo vgcreate vg1 /dev/$physical_device
 sudo lvcreate -l 100%FREE -n lv1 vg1
 sudo mkfs.ext4 /dev/vg1/lv1
 sudo mkdir /datadrive
