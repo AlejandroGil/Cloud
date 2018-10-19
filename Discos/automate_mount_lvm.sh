4119405dfc54c9505e428826905bef00e5fdff86
Alejandro Gil <agil@minsait.com>
2018-10-19 12:46:05 +0200

    some files

--- Discos/automate_mount_lvm.sh
+++ Discos/automate_mount_lvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 physical_device=$(sudo grep SCSI /var/log/syslog | tail -1 | grep -i attached | awk '{print $10}' | tr -d [])
+
 sudo apt-get install -y lvm2
 sudo vgcreate vg1 /dev/$physical_device
 sudo lvcreate -l 100%FREE -n lv1 vg1
