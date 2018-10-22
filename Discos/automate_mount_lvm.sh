117d2340e3460d6e78186265fd3dfcf44c52e23a
Alejandro Gil <agil@minsait.com>
2018-10-22 09:54:27 +0200

    [Azure disks] added comments

--- Discos/automate_mount_lvm.sh
+++ Discos/automate_mount_lvm.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#get device to mount
 physical_device=$(sudo grep SCSI /var/log/syslog | tail -1 | grep -i attached | awk '{print $10}' | tr -d [])
 
+#install lv2 in case it is not installed
 sudo apt-get install -y lvm2
-sudo vgcreate vg1 /dev/$physical_device
+
+#create volumegroup and logicalvolume
+sudo vgcreate vg1 /dev/"$physical_device"
 sudo lvcreate -l 100%FREE -n lv1 vg1
 sudo mkfs.ext4 /dev/vg1/lv1
 sudo mkdir /datadrive
 sudo mount /dev/mapper/vg1-lv1 /datadrive/
 fstab="/dev/mapper/vg1-lv1 /datadrive ext4 defaults 0 0"
-echo $fstab | sudo tee --append /etc/fstab
+echo "$fstab" | sudo tee --append /etc/fstab
