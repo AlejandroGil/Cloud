d67410ea145daaff5599c54613ebab71ba543aed
Alejandro Gil <agil@minsait.com>
2018-11-23 10:39:28 +0100

    Disk mount centos

---
+++ Discos/automate_mount_lvm_centos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#get device to mount
+physical_device=$(sudo grep SCSI /var/log/messages | tail -1 | grep -i attached | awk '{print $10}' | tr -d [])
+
+#install lv2 in case it is not installed
+sudo yum install -y lvm2
+
+#create volumegroup and logicalvolume
+sudo vgcreate vg1 /dev/"$physical_device"
+sudo lvcreate -l 100%FREE -n lv1 vg1
+sudo mkfs.ext4 /dev/vg1/lv1
+sudo mkdir /datadrive
+sudo mount /dev/mapper/vg1-lv1 /datadrive/
+fstab="/dev/mapper/vg1-lv1 /datadrive ext4 defaults 0 0"
+echo "$fstab" | sudo tee --append /etc/fstab
